f9ba46a7b4828c21265ec05f306f6c609d107dd6
Douglas Slingerland <Douglas@arkwebservices.com>
2024-02-29 14:41:01 -0500

    fix mobile menu

--- components/Layout/Header/Header.js
+++ components/Layout/Header/Header.js
@@ -15,7 +15,7 @@ export default function Header({ navColor, logo, scroll, navToggled, navHandle }
                             <Logo logo={logo} />
                         </div>
                         <Menu navColor={navColor} />
-                        <MenuMobile navToggled={navToggled} />
+                        <MenuMobile navColor={navColor} navToggled={navToggled} />
 
                         <div className="col-auto">
                             <div className="header__right">
@@ -25,12 +25,12 @@ export default function Header({ navColor, logo, scroll, navToggled, navHandle }
                                 </div>
                                 <div className="header__menu justify-center items-center relative d-none md:d-flex" onClick={navHandle}>
                                     <button type="button" className={`d-flex items-center justify-center js-nav-open ${navToggled ? "pointer-events-none opac-0" : "opac-1"}`} onClick={navHandle}>
-                                        <Icon.Menu className="text-black size-32 str-width-1"/>
+                                        <Icon.Menu className="text-black size-32 str-width-1" />
                                     </button>
                                     <button type="button"
                                         className={`d-flex items-center justify-center absolute-y-center right-0  js-nav-close ${navToggled ? "pointer-events-none opac-1" : "opac-0"}`}>
                                         Close
-                                        <Icon.X className="text-black size-32 str-width-1 ml-4"/>
+                                        <Icon.X className="text-black size-32 str-width-1 ml-4" />
                                     </button>
                                 </div>
                             </div>
