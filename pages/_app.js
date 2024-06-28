013153371aaf14c2e3d6b9dce69fd9d6b6e0bc6b
Douglas Slingerland <Douglas@arkwebservices.com>
2024-06-28 18:19:01 -0400

    add pixel

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,19 @@
+import Head from 'next/head';
 import NextNProgress from 'nextjs-progressbar';
-import Head from 'next/head'
 
-import '../public/css/vendors.css'
-import '../public/css/animate.min.css'
-import '../public/css/main.css'
+import '../public/css/animate.min.css';
+import '../public/css/main.css';
+import '../public/css/vendors.css';
 
 import "swiper/css";
 import "swiper/css/navigation";
-import "swiper/css/navigation";
-
+import { B2BPixel } from '../b2bpixel';
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
+    <B2BPixel />
+        
         <title>Ark Web Services | Web Design & Development</title>
       </Head>
 
